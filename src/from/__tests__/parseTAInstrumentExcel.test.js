89ea2452e15781129851164d74b46d31ee4c4836
Kevin <kevin.jablonka@epfl.ch>
2020-09-21 12:31:59 +0200

    feat: added excel parser, #9

--- src/from/__tests__/parseTAInstrumentExcel.test.js
+++ src/from/__tests__/parseTAInstrumentExcel.test.js
@@ -5,7 +5,7 @@ import { parseTAInstrumentsExcel, testables } from '../parseTAInstrumentsExcel';
 
 let xlsx = require('xlsx');
 
-const { parseMeta } = testables;
+const { parseMeta, parseDataSheet } = testables;
 
 describe('TA instrument excel parser', () => {
   let data = readFileSync(
@@ -20,5 +20,29 @@ describe('TA instrument excel parser', () => {
     expect(meta.procedure).toHaveLength(12);
     expect(meta.sampleWeight).toStrictEqual(5.977);
     expect(meta.sampleWeightUnit).toStrictEqual('mg');
+  });
+
+  it('parse data', () => {
+    let data = parseDataSheet(workbook.Sheets['Isothermal 5.0 min']);
+    expect(data[0][0]).toStrictEqual(45.4783);
+    expect(data[1][0]).toStrictEqual(52.15);
+    expect(data[2][0]).toStrictEqual(5.857);
+    expect(data[3][0]).toStrictEqual(97.997);
+    expect(data[0]).toHaveLength(2999);
+    expect(data[1]).toHaveLength(2999);
+    expect(data[2]).toHaveLength(2999);
+    expect(data[3]).toHaveLength(2999);
+    expect(data[0][2998]).toStrictEqual(50.475);
+    expect(data[1][2998]).toStrictEqual(49.92);
+    expect(data[2][2998]).toStrictEqual(5.858);
+    expect(data[3][2998]).toStrictEqual(98.015);
+
+    let data2 = parseDataSheet(workbook.Sheets['Isothermal 5.0 min-2']);
+    expect(data2[0][2997]).toStrictEqual(119.39);
+  });
+
+  it('parse all sheets', () => {
+    let output = parseTAInstrumentsExcel(data);
+    expect(output.time).toHaveLength(71627);
   });
 });
--- src/from/parseTAInstrumentsExcel.js
+++ src/from/parseTAInstrumentsExcel.js
@@ -4,6 +4,29 @@ function valueElseUndefined(cell) {
   return cell ? cell.v : undefined;
 }
 
+function valueElseUndefinedFloat(cell) {
+  return cell ? parseFloat(cell.v) : undefined;
+}
+
+function parseDataSheet(sheet) {
+  let range = xlsx.utils.decode_range(sheet['!ref']);
+  let data = [[], [], [], []]; // we assume that it is time, temperature, weight, weight %
+  let rowNum;
+  let colNum;
+  for (rowNum = range.s.r + 4; rowNum <= range.e.r; rowNum++) {
+    // the first row seems to always be constant
+    for (colNum = 0; colNum <= 3; colNum++) {
+      data[colNum].push(
+        valueElseUndefinedFloat(
+          sheet[xlsx.utils.encode_cell({ r: rowNum, c: colNum })],
+        ),
+      );
+    }
+  }
+
+  return data;
+}
+
 function parseMeta(detailsSheet) {
   let meta = {};
   meta.fileName = valueElseUndefined(detailsSheet.B1);
@@ -24,11 +47,35 @@ function parseMeta(detailsSheet) {
   return meta;
 }
 
-export function parseTAInstrumentsExcel(data) {
-  const workbook = xlsx.read(data);
+export function parseTAInstrumentsExcel(inputData) {
+  const workbook = xlsx.read(inputData);
   let meta = parseMeta(workbook.Sheets.Details);
+
+  let data = {
+    time: [],
+    temperature: [],
+    weight: [],
+    weightPercent: [],
+  };
+
+  let parsedData;
+
+  workbook.SheetNames.forEach((sheetName) => {
+    if (sheetName !== 'Details') {
+      parsedData = parseDataSheet(workbook.Sheets[sheetName]);
+      data.time.push(...parsedData[0]);
+      data.temperature.push(...parsedData[1]);
+      data.weight.push(...parsedData[2]);
+      data.weightPercent.push(...parsedData[3]);
+    }
+  });
+
+  data.meta = meta;
+
+  return data;
 }
 
 export const testables = {
   parseMeta: parseMeta,
+  parseDataSheet: parseDataSheet,
 };
