bd56c594758009b65f44f980eb49f561bbd8cdad
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-05-05 15:57:39 +0200

    chore: add test case for TA

--- src/from/__tests__/fromTAInstruments.test.js
+++ src/from/__tests__/fromTAInstruments.test.js
@@ -68,3 +68,38 @@ test('fromTAInstruments test 2', () => {
   expect(spectrum.variables.y.data).toHaveLength(18995);
   expect(spectrum.meta.methodSteps).toHaveLength(3);
 });
+
+test('fromTAInstruments test 3', () => {
+  let file = readFileSync(
+    join(
+      __dirname,
+      '../../../testFiles/engarcia_CPR-11_JCRN_180925-cpr-1=1@100c.txt',
+    ),
+    'utf16le',
+  );
+
+  const analysis = fromTAInstruments(file);
+
+  const jcamp = toJcamp(analysis)
+    .split(/\r?\n/)
+    .filter((line) => line.match(/##UNIT|##VAR_TYPE|##SYMBOL|##VAR_NAME/));
+
+  expect(jcamp).toStrictEqual([
+    '##VAR_NAME=  Program temperature,Weight,Time',
+    '##SYMBOL=    x,y,t',
+    '##VAR_TYPE=  DEPENDENT,DEPENDENT,INDEPENDENT',
+    '##UNITS=     °C,mg,min',
+  ]);
+
+  const spectrum = analysis.getXYSpectrum();
+
+  expect(Object.keys(spectrum.variables)).toStrictEqual(['x', 'y']);
+
+  expect(spectrum.meta.sampleName).toBe('180925_CPR 1-1@100C');
+  expect(spectrum.title).toBe('180925_CPR 1-1@100C');
+  expect(spectrum.variables.x.data[0]).toStrictEqual(40.59913);
+  expect(spectrum.variables.x.data).toHaveLength(11857);
+  expect(spectrum.variables.x.data[11856]).toStrictEqual(49.58992);
+  expect(spectrum.variables.y.data).toHaveLength(11857);
+  expect(spectrum.meta.methodSteps).toHaveLength(13);
+});
