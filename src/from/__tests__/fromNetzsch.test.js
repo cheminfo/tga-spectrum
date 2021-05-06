1fbe2bd518233d930138ad36141fd796380ca4f4
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-05-06 11:33:42 +0200

    fix: mass in netzsch parser

--- src/from/__tests__/fromNetzsch.test.js
+++ src/from/__tests__/fromNetzsch.test.js
@@ -11,10 +11,12 @@ test('fromNetzsch', () => {
   expect(result.spectra).toHaveLength(1);
   const spectrum = result.getSpectrum();
   expect(Object.keys(spectrum.variables)).toStrictEqual(['x', 'y', 't']);
+  expect(spectrum.variables.x.data[0]).toStrictEqual(27.141);
+  expect(spectrum.variables.y.data[0]).toStrictEqual(890.9062668);
   expect(spectrum.variables.x.data).toHaveLength(270);
   expect(spectrum.variables.y.data).toHaveLength(270);
   expect(spectrum.variables.t.data).toHaveLength(270);
   let jcamp = toJcamp(result);
   expect(jcamp.length).toBeGreaterThan(7500);
-  expect(jcamp.length).toBeLessThan(8500);
+  expect(jcamp.length).toBeLessThan(9200);
 });
